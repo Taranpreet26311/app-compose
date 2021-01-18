c760e76b1c07125603b8748f9fa817bcc5ca6048
Taranpreet26311 <taransingh26311@gmail.com>
2021-01-18 18:52:25 +0530

    pusing changes

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # app-compose
+
+A project to deploy a flask app connected with Redis using docker-compose. If use the existing images that are avaiable in public registry or build your own images ans use those instead, by uncommenting "build" and commenting "image" line docker-compose
+
+To deploy it use the below command
+
+`docker-compose up`
---
+++ flask2/main.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from redis import Redis
+import os
+app = Flask(__name__)
+HOST = os.getenv('HOST', 'redis')
+redis = Redis(host=HOST, port=6379)
+
+@app.route('/')
+def hello():
+    count = redis.incr('hits')
+    return 'Hello from Docker, This is Flask 2! I have been seen {} times.\n'.format(count)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
